1229e0194ba8760664ad24d5a61f9551297051ee
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-02-20 01:50:33 -0800

    Set up git submodules when installing versioned .vim.

--- install.sh
+++ install.sh
@@ -6,3 +6,10 @@ mv .vimrc .vimrc-
 ln -sf .vim/vimrc .vimrc
 mv .gvimrc .gvimrc-
 ln -sf .vim/gvimrc .gvimrc
+
+cd ~/.vim
+
+# make sure that submodules are correctly configured
+git submodule init
+git submodule update
+git submodule sync
